218cf8c55d4603e8cb65f2ad98c4228d81cb67f2
Matteo Altobelli <altobelli.matteo45@gmail.com>
2017-01-23 23:54:43 +0100

    aggiornamento2
    Versione 2

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
  /* Esercizio 1
 
-                             // prendiamo elemento da html e aggiungiamo evento di lettura
-                             document.getElementById("vai").addEventListener("scroll", myFunction);
-                             // dichiariamo myFunction
-                             function myFunction() {
-                                 // prendiamo elemento html
-                                 document.getElementById("demo").innerHTML = "Complimenti hai scrollato il tu primo div";
-                             }
-                             */
+                                        // prendiamo elemento da html e aggiungiamo evento di lettura
+                                        document.getElementById("vai").addEventListener("scroll", myFunction);
+                                        // dichiariamo myFunction
+                                        function myFunction() {
+                                            // prendiamo elemento html
+                                            document.getElementById("demo").innerHTML = "Complimenti hai scrollato il tu primo div";
+                                        }
+                                        */
+ /////////////////////////////////////////////////
+ /////////////////////////////////////////////////
  /* 
  // Esercizio 3
   function hello() {
@@ -28,9 +30,25 @@
   var qui = countClicks;
   document.getElementById("qui").innerHTML("Hai cliccato" + qui);
   */
+ //////////////////////////////////////////
+ /////////////////////////////////////////
  /*
   // Esercizio 5
   function myFunction() {
       document.getElementById("myBtn").value = "Audi";
    
   }    */
+ //////////////////////////////////////////
+ /////////////////////////////////////////
+ /* Esercizio 6
+  function removeDummy() {
+      var elem = document.getElementById('dummy');
+      elem.parentNode.removeChild(elem);
+  } */
+ //////////////////////////////////////////
+ /////////////////////////////////////////
+ document.addEventListener('click', function (e) {
+     e = e || window.event;
+     var target = e.target || e.srcElement
+         , text = target.textContent || text.innerText;
+ }, false);
