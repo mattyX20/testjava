1c518088a64beedc63ed2afbbdf766cd5d8397d1
Matteo Altobelli <altobelli.matteo45@gmail.com>
2017-01-23 22:07:06 +0100

    Esercizio
    file

---
+++ test.js
@@ -0,0 +1,36 @@
+ /* Esercizio 1
+
+                             // prendiamo elemento da html e aggiungiamo evento di lettura
+                             document.getElementById("vai").addEventListener("scroll", myFunction);
+                             // dichiariamo myFunction
+                             function myFunction() {
+                                 // prendiamo elemento html
+                                 document.getElementById("demo").innerHTML = "Complimenti hai scrollato il tu primo div";
+                             }
+                             */
+ /* 
+ // Esercizio 3
+  function hello() {
+      alert("Hello there!");
+  }
+  var countClicks = 0;
+
+  function upCount() {
+      if (document.getElementById('hello').onclick = "hello()") {
+          countClicks++;
+          alert(countClicks);
+          return true;
+      }
+      else {
+          return false;
+      }
+  };
+  var qui = countClicks;
+  document.getElementById("qui").innerHTML("Hai cliccato" + qui);
+  */
+ /*
+  // Esercizio 5
+  function myFunction() {
+      document.getElementById("myBtn").value = "Audi";
+   
+  }    */
